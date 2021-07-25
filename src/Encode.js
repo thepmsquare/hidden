0ff462c3865bb374c7c7a1da48224ee2e5e06b21
Parth Mangtani <impmmmm@gmail.com>
2021-07-26 03:09:16 +0530

    set up logic for encode

--- src/Encode.js
+++ src/Encode.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
-import { PrimaryButton, TextField, DefaultButton } from "@fluentui/react";
+import {
+  PrimaryButton,
+  TextField,
+  DefaultButton,
+  TeachingBubble,
+} from "@fluentui/react";
 import { Icon } from "@fluentui/react/lib/Icon";
 import "./stylesheets/Encode.css";
 
@@ -8,6 +13,8 @@ class Encode extends Component {
     super(props);
     this.state = {
       selectedImage: null,
+      isTeachingBubbleVisible: false,
+      message: "",
     };
   }
 
@@ -29,9 +36,57 @@ class Encode extends Component {
     };
   };
 
+  hideTeachingBubble = () => {
+    this.setState(() => {
+      return {
+        isTeachingBubbleVisible: false,
+      };
+    });
+  };
+
+  onInputChange = (e) => {
+    this.setState(() => {
+      return {
+        message: e.target.value,
+      };
+    });
+  };
+
+  sendAPIRequest = async (e) => {
+    e.preventDefault();
+    if (this.state.selectedImage) {
+      // const url = "https://hiddenapi.herokuapp.com/encode/";
+      const url = "http://127.0.0.1:8000/encode";
+      const params = {
+        image: this.state.selectedImage,
+        message: this.state.message,
+      };
+
+      const response = await fetch(url, {
+        method: "post",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
+      const data = await response.json();
+      console.log(data);
+    } else {
+      this.setState(() => {
+        return {
+          isTeachingBubbleVisible: true,
+        };
+      });
+    }
+  };
+
   render = () => {
     return (
-      <form className="Encode">
+      <form
+        className="Encode"
+        onSubmit={this.sendAPIRequest}
+        acceptCharset="utf-8"
+      >
         {this.state.selectedImage ? (
           <img
             src={this.state.selectedImage}
@@ -48,7 +103,7 @@ class Encode extends Component {
           </div>
         )}
 
-        <DefaultButton onClick={this.uploadPhoto}>
+        <DefaultButton onClick={this.uploadPhoto} id="uploadPhotoButton">
           {this.state.selectedImage ? "Change" : "Upload"} Photo
         </DefaultButton>
         <TextField
@@ -57,9 +112,22 @@ class Encode extends Component {
           className="Encode-messageInput"
           resizable={false}
           multiline
+          value={this.state.message}
+          onChange={this.onInputChange}
         ></TextField>
 
-        <PrimaryButton className="Encode-submitButton">Submit</PrimaryButton>
+        <PrimaryButton className="Encode-submitButton" type="submit">
+          Submit
+        </PrimaryButton>
+
+        {this.state.isTeachingBubbleVisible && (
+          <TeachingBubble
+            target="#uploadPhotoButton"
+            hasSmallHeadline={true}
+            onDismiss={this.hideTeachingBubble}
+            headline="Upload a Photo to encode message."
+          ></TeachingBubble>
+        )}
       </form>
     );
   };
