af7aec776918315125da14f490dd8c4e8ad9e273
Parth Mangtani <impmmmm@gmail.com>
2023-09-17 00:11:22 +0530

    changes

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -5,9 +5,6 @@ const config: GatsbyConfig = {
     title: `hidden`,
     siteUrl: `https://thepmsquare.github.io/hidden`,
   },
-  module.exports = {
-    pathPrefix: "/hidden",
-  }
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
