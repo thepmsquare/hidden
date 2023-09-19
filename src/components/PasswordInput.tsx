a60c954f354454f489d389cde1802cd1e8568a5d
thepmsquare <impmmmm@gmail.com>
2023-09-19 19:22:59 +0530

    Update PasswordInput.tsx

--- src/components/PasswordInput.tsx
+++ src/components/PasswordInput.tsx
@@ -2,6 +2,8 @@ import React, { ChangeEventHandler, useState } from "react";
 import {
   FormControl,
   InputLabel,
+  Input,
+  FilledInput,
   OutlinedInput,
   InputAdornment,
   IconButton,
@@ -29,27 +31,75 @@ const PasswordInput = (props: {
   return (
     <FormControl variant={props.variant}>
       <InputLabel htmlFor={props.uniqueIdForARIA}>{props.label}</InputLabel>
-      <OutlinedInput
-        id={props.uniqueIdForARIA}
-        type={showPassword ? "text" : "password"}
-        value={props.value}
-        onChange={props.onChange}
-        {...props.others}
-        autoComplete="off"
-        endAdornment={
-          <InputAdornment position="end">
-            <IconButton
-              aria-label="toggle password visibility"
-              onClick={handleClickShowPassword}
-              onMouseDown={handleMouseDownPassword}
-              edge="end"
-            >
-              {showPassword ? <VisibilityOff /> : <Visibility />}
-            </IconButton>
-          </InputAdornment>
-        }
-        label={props.label}
-      />
+      {props.variant === "filled" && (
+        <FilledInput
+          id={props.uniqueIdForARIA}
+          type={showPassword ? "text" : "password"}
+          value={props.value}
+          onChange={props.onChange}
+          {...props.others}
+          autoComplete="off"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label={props.label}
+        />
+      )}
+      {props.variant === "outlined" && (
+        <OutlinedInput
+          id={props.uniqueIdForARIA}
+          type={showPassword ? "text" : "password"}
+          value={props.value}
+          onChange={props.onChange}
+          {...props.others}
+          autoComplete="off"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label={props.label}
+        />
+      )}
+      {props.variant === "standard" && (
+        <Input
+          id={props.uniqueIdForARIA}
+          type={showPassword ? "text" : "password"}
+          value={props.value}
+          onChange={props.onChange}
+          {...props.others}
+          autoComplete="off"
+          endAdornment={
+            <InputAdornment position="end">
+              <IconButton
+                aria-label="toggle password visibility"
+                onClick={handleClickShowPassword}
+                onMouseDown={handleMouseDownPassword}
+                edge="end"
+              >
+                {showPassword ? <VisibilityOff /> : <Visibility />}
+              </IconButton>
+            </InputAdornment>
+          }
+          label={props.label}
+        />
+      )}
     </FormControl>
   );
 };
